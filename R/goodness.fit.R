bc1f08b72597850d880595185295147150ea4503
prdm0 <pedro.rafael.marinho@gmail.com>
2019-04-17 07:47:04 -0300

    improving the indentation (goodness.fit function)

--- R/goodness.fit.R
+++ R/goodness.fit.R
@@ -2,45 +2,45 @@ goodness.fit <-
 function(pdf, cdf, starts, data, method = "PSO", domain = c(0,Inf),
                          mle = NULL,...){
   
-  if(missingArg(cdf)==TRUE) stop("Unknown cumulative distribution function. The function needs to be informed.")
-  if(missingArg(pdf)==TRUE) stop("Unknown probability density function. The function needs to be informed.")
-  if(class(pdf)!="function") stop("The argument pdf must be a function. See the example in the documentation!")
-  if(class(cdf)!="function") stop("The argument cdf must be a function. See the example in the documentation!")
-  if(missingArg(data)==TRUE) stop("Database missing!")
-  if(TRUE%in%is.nan(data)==TRUE) warning("The data have missing information!")
-  if(length(domain)!=2) stop("The domain must have two arguments!")
+  if(missingArg(cdf) == TRUE) stop("Unknown cumulative distribution function. The function needs to be informed.")
+  if(missingArg(pdf) == TRUE) stop("Unknown probability density function. The function needs to be informed.")
+  if(class(pdf) != "function") stop("The argument pdf must be a function. See the example in the documentation!")
+  if(class(cdf) != "function") stop("The argument cdf must be a function. See the example in the documentation!")
+  if(missingArg(data) == TRUE) stop("Database missing!")
+  if(TRUE%in%is.nan(data) == TRUE) warning("The data have missing information!")
+  if(length(domain) != 2) stop("The domain must have two arguments!")
   
-  if(is.null(mle)==TRUE){
-    if(missingArg(starts)==TRUE) stop("The initial shots were not informed!")
+  if(is.null(mle) == TRUE){
+    if(missingArg(starts) == TRUE) stop("The initial shots were not informed!")
   }else{
     starts = mle 
   }   
   
   # Verifying properties of cumulative distribution function.
   
-  if(cdf(par=starts, x = domain[2])!=1) warning("The cdf function informed is not a cumulative distribution function! The function no takes value 1 in Inf.")
-  if(cdf(par=starts, x = domain[1])!=0) warning("Check if the cumulative distribution informed is actually a distribution function.")
+  if(cdf(par=starts, x = domain[2]) != 1) warning("The cdf function informed is not a cumulative distribution function! The function no takes value 1 in Inf.")
+  if(cdf(par=starts, x = domain[1]) != 0) warning("Check if the cumulative distribution informed is actually a distribution function.")
   
-  myintegrate = function(...) tryCatch(integrate(...), error=function(e) NA)
+  myintegrate = function(...) tryCatch(integrate(...), error = function(e) NA)
   
-  value_int = as.numeric(myintegrate(f=pdf,par=starts,lower=domain[1],
-                                     upper=domain[2])[1])
-  if(isTRUE(is.na(value_int))==TRUE) warning("Make sure that pdf is a probability density function. The integral in the domain specified is not 					     convergent.")
+  value_int = as.numeric(myintegrate(f = pdf, par = starts, lower = domain[1],
+                                     upper = domain[2])[1])
+  if(isTRUE(is.na(value_int)) == TRUE) warning("Make sure that pdf is a probability density function. The integral in the domain specified is not 					     convergent.")
   
-  if(isTRUE(is.na(value_int))!=TRUE){
-     #Verifying properties of probability density function.
-    if(value_int<0.90){
+  if(isTRUE(is.na(value_int)) != TRUE){
+     # Verifying properties of probability density function.
+    if(value_int < 0.90){
       warning("The integral from ", domain[1], " to ", domain[2]," of the probability density function has different from 1. Make sure the option 		      domain is correct.")
     }
     
-    if(round(value_int)!=1){
+    if(round(value_int) != 1){
       warning("pdf is not a probability density function.")
     }
   }
   
-  if(is.null(mle)==TRUE){    
-    likelihood = function(par,x){
-      -sum(log(pdf(par,x)))
+  if(is.null(mle) == TRUE){    
+    likelihood = function(par, x){
+      -sum(log(pdf(par, x)))
     }
     
     if(method == "PSO" || method == "P"){
@@ -80,8 +80,8 @@ function(pdf, cdf, starts, data, method = "PSO", domain = c(0,Inf),
     n = length(data) # Tamanho da amostra.
     
     y = qnorm(v) # Inversa da acumulada da normal.
-    y[which(y==Inf)] = 10
-    u = pnorm((y-mean(y))/sqrt(var(y)))
+    y[which(y == Inf)] = 10
+    u = pnorm((y - mean(y))/sqrt(var(y)))
     
     W_temp <- vector()
     A_temp <- vector()
@@ -97,17 +97,17 @@ function(pdf, cdf, starts, data, method = "PSO", domain = c(0,Inf),
     A_star = A_2*(1+0.75/n + 2.25/n^2)
     
     p = length(parameters)
-    log.likelihood = -1*likelihood(parameters,data) 
+    log.likelihood = -1*likelihood(parameters, data) 
     AICc = -2*log.likelihood + 2*p + 2*(p*(p+1))/(n-p-1)
     AIC  = -2*log.likelihood + 2*p
     BIC  = -2*log.likelihood + p*log(n)
     HQIC = -2*log.likelihood + 2*log(log(n))*p
     ks.testg = function(...) tryCatch(ks.test(...),
                                       warning = function(war) NA)
-    KS = ks.test(x = data, y= "cdf", par = as.vector(parameters))
+    KS = ks.test(x = data, y = "cdf", par = as.vector(parameters))
     
     
-    if(method=="PSO" || method=="P"){
+    if(method == "PSO" || method == "P"){
       result = (list("W" = W_star,"A" = A_star, "KS" = KS,
                      "mle" = parameters, "AIC" = AIC ,"CAIC " = AICc,
                      "BIC" = BIC, "HQIC" = HQIC, "Value" = result$f[length(result$f)]))
@@ -126,15 +126,15 @@ function(pdf, cdf, starts, data, method = "PSO", domain = c(0,Inf),
   if(class(mle)=="numeric"){
     
     likelihood = function(par,x){
-      -sum(log(pdf(par,x)))
+      -sum(log(pdf(par, x)))
     }
     
     parameters = mle
     data_orderdenados = sort(data)
-    v = cdf(as.vector(parameters),data_orderdenados) # Dados ordenados.
+    v = cdf(as.vector(parameters), data_orderdenados) # Dados ordenados.
     n = length(data) # Tamanho da amostra.
     y = qnorm(v) # Inversa da acumulada da normal.
-    y[which(y==Inf)] = 10
+    y[which(y == Inf)] = 10
     u = pnorm((y-mean(y))/sqrt(var(y)))
     
     W_temp <- vector()
@@ -151,14 +151,14 @@ function(pdf, cdf, starts, data, method = "PSO", domain = c(0,Inf),
     A_star = A_2*(1+0.75/n + 2.25/n^2)
     
     p = length(parameters)
-    log.likelihood = -1*likelihood(parameters,data) 
+    log.likelihood = -1*likelihood(parameters, data) 
     AICc = -2*log.likelihood + 2*p + 2*(p*(p+1))/(n-p-1)
     AIC  = -2*log.likelihood + 2*p
     BIC  = -2*log.likelihood + p*log(n)
     HQIC = -2*log.likelihood + 2*log(log(n))*p
     ks.testg = function(...) tryCatch(ks.test(...),
                                       warning = function(war) NA)
-    KS = ks.test(x = data, y= "cdf", par = as.vector(parameters))
+    KS = ks.test(x = data, y = "cdf", par = as.vector(parameters))
     
     result = (list("W" = W_star,"A" = A_star, "KS" = KS, "AIC" = AIC,
                       "CAIC" = AICc, "BIC" = BIC, "HQIC" = HQIC))
