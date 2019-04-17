660905b3c1b3da8440f29513c60ac2454c0d1775
prdm0 <pedro.rafael.marinho@gmail.com>
2019-04-17 08:16:21 -0300

    add L-BFGS-B method in goodness.fit function

--- R/goodness.fit.R
+++ R/goodness.fit.R
@@ -1,4 +1,4 @@
-goodness.fit <- function(pdf, cdf, starts, data, method = "PSO", domain = c(0,Inf),
+goodness.fit <- function(pdf, cdf, starts, data, method = "PSO",
                          mle = NULL, ...){
    
   if(missingArg(cdf) == TRUE) stop("Unknown cumulative distribution function. The function needs to be informed.")
@@ -17,25 +17,25 @@ goodness.fit <- function(pdf, cdf, starts, data, method = "PSO", domain = c(0,In
   
   # Verifying properties of cumulative distribution function.
   
-  if(cdf(par = starts, x = domain[2]) != 1) warning("The cdf function informed is not a cumulative distribution function! The function no takes value 1 in Inf.")
-  if(cdf(par = starts, x = domain[1]) != 0) warning("Check if the cumulative distribution informed is actually a distribution function.")
-  
-  myintegrate = function(...) tryCatch(integrate(...), error = function(e) NA)
-  
-  value_int = as.numeric(myintegrate(f = pdf, par = starts, lower = domain[1],
-                                     upper = domain[2])[1])
-  if(isTRUE(is.na(value_int)) == TRUE) warning("Make sure that pdf is a probability density function. The integral in the domain specified is not 					     convergent.")
-  
-  if(isTRUE(is.na(value_int)) != TRUE){
-     # Verifying properties of probability density function.
-    if(value_int < 0.90){
-      warning("The integral from ", domain[1], " to ", domain[2]," of the probability density function has different from 1. Make sure the option 		      domain is correct.")
-    }
-    
-    if(round(value_int) != 1){
-      warning("pdf is not a probability density function.")
-    }
-  }
+  # if(cdf(par = starts, x = domain[2]) != 1) warning("The cdf function informed is not a cumulative distribution function! The function no takes value 1 in Inf.")
+  # if(cdf(par = starts, x = domain[1]) != 0) warning("Check if the cumulative distribution informed is actually a distribution function.")
+  #
+  #myintegrate = function(...) tryCatch(integrate(...), error = function(e) NA)
+  #
+  #value_int = as.numeric(myintegrate(f = pdf, par = starts, lower = domain[1],
+  #                                   upper = domain[2])[1])
+  #if(isTRUE(is.na(value_int)) == TRUE) warning("Make sure that pdf is a probability density function. The integral in the domain specified is not 					     convergent.")
+  #
+  # if(isTRUE(is.na(value_int)) != TRUE){
+  #    # Verifying properties of probability density function.
+  #   if(value_int < 0.90){
+  #     warning("The integral from ", domain[1], " to ", domain[2]," of the probability density function has different from 1. Make sure the option 		      domain is correct.")
+  #   }
+  #   
+  #   if(round(value_int) != 1){
+  #     warning("pdf is not a probability density function.")
+  #   }
+  # }
   
   if(is.null(mle) == TRUE){    
     likelihood = function(par, x){
@@ -43,32 +43,51 @@ goodness.fit <- function(pdf, cdf, starts, data, method = "PSO", domain = c(0,In
     }
     
     if(method == "PSO" || method == "P"){
-      result = pso(func = likelihood, data = data,...)
+      
+      result = pso(func = likelihood, data = data, ...)
+      
     }
     
     if(method == "Nelder-Mead" || method == "N"){
+      
       result = optim(par = starts, fn = likelihood, x = data,
-                        method = "Nelder-Mead", hessian = TRUE)
+                     method = "Nelder-Mead", hessian = TRUE)
+      
     }
     
     if(method == "CG" || method == "C"){
+      
       result = optim(par = starts, fn = likelihood, x = data,
-                        method = "CG", hessian = TRUE)
+                     method = "CG", hessian = TRUE)
+      
     }  
     
     if(method == "SANN" || method == "S"){
+      
       result = optim(par = starts, fn = likelihood, x = data,
-                        method = "SANN", hessian = TRUE)
+                     method = "SANN", hessian = TRUE)
+      
     }  
     
     if(method == "BFGS" || method == "B"){
+      
       result = optim(par = starts, fn = likelihood, x = data,
-                        method = "BFGS", hessian = TRUE)
+                     method = "BFGS", hessian = TRUE)
+      
+    }
+    
+    if(method == "L-BFGS-B" || method == "L"){
+      
+      result = optim(par = starts, fn = likelihood, x = data,
+                     method = "L-BFGS-B", hessian = TRUE, ...)
+      
     }
     
     if((FALSE %in% (method != c("PSO", "L", "BFGS", "B",
-                                "Nelder-Mead", "P", "N", "SANN", "S", "CG", "C")))==FALSE){
-      stop("Valid options are: PSO or P, BFGS or B, Nelder-Mead or N, SANN or S, CG or C.")
+                                "Nelder-Mead", "P", "N", "SANN", "S", "CG", "C"))) == FALSE){
+      
+      stop("Valid options are: PSO or P, BFGS or B, L-BFGS-B or L, Nelder-Mead or N, SANN or S, CG or C.")
+      
     }
     
     parameters = result$par
@@ -96,29 +115,38 @@ goodness.fit <- function(pdf, cdf, starts, data, method = "PSO", domain = c(0,In
     A_star = A_2 * (1 + 0.75/n + 2.25/n^2)
     
     p = length(parameters)
+    
     log.likelihood = -1 * likelihood(parameters, data) 
-    AICc = -2 * log.likelihood + 2*p + 2*(p*(p+1))/(n-p-1)
-    AIC  = -2 * log.likelihood + 2*p
-    BIC  = -2 * log.likelihood + p*log(n)
-    HQIC = -2 * log.likelihood + 2*log(log(n))*p
+    
+    AICc = -2 * log.likelihood + 2 * p + 2 * (p*(p+1))/(n-p-1)
+    AIC  = -2 * log.likelihood + 2 * p
+    BIC  = -2 * log.likelihood + p * log(n)
+    HQIC = -2 * log.likelihood + 2 * log(log(n)) * p
+    
     ks.testg = function(...) tryCatch(ks.test(...),
                                       warning = function(war) NA)
+    
     KS = ks.test(x = data, y = "cdf", par = as.vector(parameters))
     
     
     if(method == "PSO" || method == "P"){
+      
       result = (list("W" = W_star,"A" = A_star, "KS" = KS,
                      "mle" = parameters, "AIC" = AIC , "CAIC " = AICc,
                      "BIC" = BIC, "HQIC" = HQIC, "Value" = result$f[length(result$f)]))
       class(result) <- "list" 
       return(result)
+      
     }else{
+      
       result = (list("W" = W_star, "A" = A_star, "KS" = KS,
                      "mle" = parameters, "AIC" = AIC , "CAIC " = AICc,
                      "BIC" = BIC, "HQIC" = HQIC, "Erro" = sqrt(diag(solve(hessiana))),
                      "Value" = result$value, "Convergence" = result$convergence))
+      
       class(result) <- "list" 
       return(result)
+      
     }
   }
   
@@ -140,8 +168,10 @@ goodness.fit <- function(pdf, cdf, starts, data, method = "PSO", domain = c(0,In
     A_temp <- vector()
     
     for(i in 1:n){
+      
       W_temp[i] = (u[i] - (2*i-1)/(2*n))^2
       A_temp[i] = (2*i-1)*log(u[i]) + (2*n+1-2*i)*log(1-u[i])
+      
     }
     
     A_2 = -n - mean(A_temp)
@@ -150,17 +180,21 @@ goodness.fit <- function(pdf, cdf, starts, data, method = "PSO", domain = c(0,In
     A_star = A_2 * (1+0.75/n + 2.25/n^2)
     
     p = length(parameters)
+    
     log.likelihood = -1 * likelihood(parameters, data) 
-    AICc = -2 * log.likelihood + 2*p + 2*(p*(p+1))/(n-p-1)
-    AIC  = -2 * log.likelihood + 2*p
-    BIC  = -2 * log.likelihood + p*log(n)
-    HQIC = -2 * log.likelihood + 2*log(log(n))*p
+    
+    AICc = -2 * log.likelihood + 2 * p + 2 * (p*(p+1))/(n-p-1)
+    AIC  = -2 * log.likelihood + 2 * p
+    BIC  = -2 * log.likelihood + p * log(n)
+    HQIC = -2 * log.likelihood + 2 * log(log(n)) * p
     ks.testg = function(...) tryCatch(ks.test(...),
                                       warning = function(war) NA)
+    
     KS = ks.test(x = data, y = "cdf", par = as.vector(parameters))
     
     result = (list("W" = W_star,"A" = A_star, "KS" = KS, "AIC" = AIC,
-                      "CAIC" = AICc, "BIC" = BIC, "HQIC" = HQIC))
+                   "CAIC" = AICc, "BIC" = BIC, "HQIC" = HQIC))
+    
     class(result) <- "list" 
     return(result)
   }
